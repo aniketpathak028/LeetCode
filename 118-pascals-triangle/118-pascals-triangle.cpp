d4633e10efcdafe94e08061dc398e14fe489e230
Aniket Pathak <74519511+aniketpathak028@users.noreply.github.com>
2022-02-16 22:33:58 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (91.63%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -1,40 +1,12 @@
 class Solution {
 public:
-    void p(int i, vector<vector<int>> &ans, int n){
-        if(i==n) return;
-        if(i==0) {
-           vector<int> row; 
-           row.push_back(1);
-           ans.push_back(row);
-           return p(1, ans, n);
+   vector<vector<int>> generate(int n) {
+        vector<vector<int>> ans(n);             // initialize n rows
+        for(int i = 0; i < n; i++) {
+            ans[i] = vector<int>(i+1,1);        // ith row(0-indexed) has i+1 elements
+            for(int j = 1; j < i; j++)          // 1st and last elements will be 1, rest will be sum of two elements from above row
+                ans[i][j] = ans[i - 1][j] + ans[i - 1][j - 1];            
         }
-        if(i==1) {
-            vector<int> row; 
-            row.push_back(1);
-            row.push_back(1);
-            ans.push_back(row);
-            return p(2, ans, n);
-        }
-        
-        vector<int> row; 
-        row.push_back(1);
-        
-        auto prev = ans[ans.size()-1];
-        
-        for(int j=0; j<prev.size()-1; j++){
-            row.push_back(prev[j]+prev[j+1]);
-        }
-        
-        row.push_back(1);
-        
-        ans.push_back(row);
-        
-        p(i+1, ans, n);
-        
-    }
-    vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> ans;
-        p(0, ans, numRows);
         return ans;
     }
 };
