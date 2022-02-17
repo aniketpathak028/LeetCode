9122fb0cc255e80077a2e0e54bf0437f99b01665
Aniket Pathak <74519511+aniketpathak028@users.noreply.github.com>
2022-02-18 00:08:15 +0530

    Time: 12 ms (28.56%), Space: 9 MB (32.82%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
-    int solve(vector<vector<int>> &triangle, int i, int j, int n, vector<vector<int>> &dp)
-    {
-        if (i == n-1)
-            return triangle[i][j];
-
-        if (dp[i][j] != -1)
-            return dp[i][j];
-
-        int d, dr;
-        d = triangle[i][j] + solve(triangle, i + 1, j, n, dp);
-        dr = triangle[i][j] + solve(triangle, i + 1, j + 1, n, dp);
-
-        return dp[i][j] = min(d, dr);
-    }
-
-
+ 
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-                 vector<vector<int>> dp(n);
-            for (int i = 0; i < n; i++)
+        vector<vector<int>> dp(n);
+        for (int i = 0; i < n; i++)
+        {
+            vector<int> row(i + 1, -1);
+            dp[i] = row;
+        }
+        for (int i = 0; i < n; i++){
+            dp[n - 1][i] = triangle[n - 1][i];
+        }
+        for (int i = n-2; i >= 0; i--)
+        {
+            for (int j = i; j >= 0; j--)
             {
-                vector<int> row(i + 1, -1);
-                dp[i] = row;
+                int d, dr;
+                d = triangle[i][j] + dp[i+1][j];
+                dr = triangle[i][j] + dp[i+1][j+1];
+
+                dp[i][j] = min(d, dr);
             }
-            return solve(triangle, 0, 0, n, dp);
+        }
+
+        return dp[0][0];
     }
 };
