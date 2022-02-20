d186f479f912f5aedec42723e33c7559fa1406e4
Aniket Pathak <74519511+aniketpathak028@users.noreply.github.com>
2022-02-20 19:32:48 +0530

    Time: 267 ms (7.72%), Space: 14.4 MB (25.63%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -3,17 +3,18 @@ public:
     int cherryPickup(vector<vector<int>>& grid) {
         int c = grid[0].size()-1;
         int r = grid.size()-1;
-        vector<vector<vector<int>>> dp(r+1, vector<vector<int>>(c+1, vector<int>(c+1, 0)));
+        vector<vector<int>> prev(vector<vector<int>>(c+1, vector<int>(c+1, 0)));
         
         // base-case intialization
         for(int j1=0; j1<=c; j1++){
             for(int j2=0; j2<=c; j2++){
-                if(j1==j2) dp[r][j1][j2]= grid[r][j1];
-                else dp[r][j1][j2] = grid[r][j1]+grid[r][j2];
+                if(j1==j2) prev[j1][j2]= grid[r][j1];
+                else prev[j1][j2] = grid[r][j1]+grid[r][j2];
             }
         }
         
         for(int i=r-1; i>=0; i--){
+            vector<vector<int>> curr(vector<vector<int>>(c+1, vector<int>(c+1, 0)));
             for(int j1=0; j1<=c; j1++){
                 for(int j2=0; j2<=c; j2++){
                     int maxi = 0;
@@ -21,19 +22,20 @@ public:
                     for(int dj2=-1; dj2<=1; dj2++){
                         if (j1 == j2){
                             if(j1+dj1>=0  && j1+dj1<=c && j2+dj2>=0 && j2+dj2<=c){
-                                maxi = max(maxi, dp[i + 1][j1 + dj1][j2 + dj2] + grid[i][j1]);
+                                maxi = max(maxi, prev[j1 + dj1][j2 + dj2] + grid[i][j1]);
                             }
                         }
                         else
                         if(j1+dj1>=0  && j1+dj1<=c && j2+dj2>=0 && j2+dj2<=c)
-                            maxi = max(maxi, dp[i + 1][j1 + dj1][j2 + dj2] + grid[i][j1] + grid[i][j2]);
+                            maxi = max(maxi, prev[j1 + dj1][j2 + dj2] + grid[i][j1] + grid[i][j2]);
                     }
                 }
-                dp[i][j1][j2]= maxi;
+                curr[j1][j2]= maxi;
                 }
             }
+            prev= curr;
         }
         
-        return dp[0][0][c];
+        return prev[0][c];
     }
 };
