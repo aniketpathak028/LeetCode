ca9da7d927d27b3633db19297066c3b8924c5f2c
Aniket Pathak <74519511+aniketpathak028@users.noreply.github.com>
2022-02-20 19:19:54 +0530

    Time: 196 ms (11.46%), Space: 14.3 MB (38.27%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,36 +1,39 @@
 class Solution {
-public:
-    int maxCherry(int i, int j1, int j2, vector<vector<int>>& grid, vector<vector<vector<int>>> &dp){
-        // base-case (out-of-bound condition)
-        if(j1<0 || j2<0 || j1>grid[0].size()-1 || j2>grid[0].size()-1)
-            return -1e8;
+public: 
+    int cherryPickup(vector<vector<int>>& grid) {
+        int c = grid[0].size()-1;
+        int r = grid.size()-1;
+        vector<vector<vector<int>>> dp(r+1, vector<vector<int>>(c+1, vector<int>(c+1, 0)));
         
-        // base-case (reached the last row)
-        if(i==grid.size()-1) {
-            if(j1==j2) return grid[i][j1];
-            else return grid[i][j1]+grid[i][j2];
-        } 
-        
-        // check if the value already exists
-        if(dp[i][j1][j2]!=-1) return dp[i][j1][j2];
-        
-        // trying all combinations and returning the maximum
-        int maxi = 0;
-        for(int dj1=-1; dj1<=1; dj1++){
-            for(int dj2=-1; dj2<=1; dj2++){
-                int value = maxCherry(i+1, j1+dj1, j2+dj2, grid, dp);
-                if(j1==j2) value += grid[i][j1];
-                else value += grid[i][j1]+grid[i][j2];
-                maxi = max(maxi, value);
+        // base-case intialization
+        for(int j1=0; j1<=c; j1++){
+            for(int j2=0; j2<=c; j2++){
+                if(j1==j2) dp[r][j1][j2]= grid[r][j1];
+                else dp[r][j1][j2] = grid[r][j1]+grid[r][j2];
             }
         }
-                             
-        return dp[i][j1][j2]=maxi;
-    }
-    int cherryPickup(vector<vector<int>>& grid) {
-        int row_length = grid[0].size()-1;
-        int cols = grid.size();
-        vector<vector<vector<int>>> dp(cols, vector<vector<int>>(row_length+1, vector<int>(row_length+1, -1)));
-        return maxCherry(0, 0, row_length, grid, dp);
+        
+        for(int i=r-1; i>=0; i--){
+            for(int j1=0; j1<=c; j1++){
+                for(int j2=0; j2<=c; j2++){
+                    int maxi = 0;
+                    for(int dj1=-1; dj1<=1; dj1++){
+                    for(int dj2=-1; dj2<=1; dj2++){
+                        if (j1 == j2){
+                            if(j1+dj1>=0  && j1+dj1<=c && j2+dj2>=0 && j2+dj2<=c){
+                                maxi = max(maxi, dp[i + 1][j1 + dj1][j2 + dj2] + grid[i][j1]);
+                            }
+                        }
+                        else
+                        if(j1+dj1>=0  && j1+dj1<=c && j2+dj2>=0 && j2+dj2<=c)
+                            maxi = max(maxi, dp[i + 1][j1 + dj1][j2 + dj2] + grid[i][j1] + grid[i][j2]);
+                    }
+                }
+                dp[i][j1][j2]= maxi;
+                }
+            }
+        }
+        
+        return dp[0][0][c];
     }
 };
