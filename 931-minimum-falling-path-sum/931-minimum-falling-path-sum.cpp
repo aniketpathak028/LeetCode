544934fcdda3e4f023553f56ca8a9ea86dc346d0
Aniket Pathak <74519511+aniketpathak028@users.noreply.github.com>
2022-02-23 20:15:37 +0530

    Time: 22 ms (42.63%), Space: 10.2 MB (38.60%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -1,33 +1,40 @@
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
-    int mini = INT_MAX;
-    int n = matrix.size();
+        int mini = INT_MAX;
+        int n = matrix.size();
 
-    // initialize DP
-    vector<vector<int>> dp(n, vector<int>(n, 0));
+        // initialize DP
+        vector<int> prev(n, 0);
 
-    // base-case initialization
-    for (int i = 0; i < n; i++) dp[n-1][i]= matrix[n-1][i];
-    
-
-    for (int i = n - 2; i >= 0; i--)
-    {
-        for (int j = 0; j < n; j++){
-            int mini = INT_MAX;
-            for (int dj = -1; dj <= 1; dj++)
-            {
-                if(j+dj>=0 && j+dj<n) mini = min(mini, dp[i + 1][j + dj] + matrix[i][j]);
-            }
-
-            dp[i][j] = mini;
+        // base-case initialization
+        for (int i = 0; i < n; i++)
+        {
+            prev[i] = matrix[n - 1][i];
         }
+
+        for (int i = n - 2; i >= 0; i--)
+        {
+            vector<int> curr(n, 0);
+            for (int j = 0; j < n; j++)
+            {
+                int mini = INT_MAX;
+                for (int dj = -1; dj <= 1; dj++)
+                {
+                   if(j+dj<n && j+dj>=0) mini = min(mini, prev[j + dj] + matrix[i][j]);
+                }
+
+                curr[j] = mini;
+            }
+            prev= curr;
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            mini = min(mini, prev[i]);
+        }
+
+        return mini;
     }
-
-
-    for (int i = 0; i < n; i++) mini = min(mini, dp[0][i]);
     
-
-    return mini;
-    }
 };
