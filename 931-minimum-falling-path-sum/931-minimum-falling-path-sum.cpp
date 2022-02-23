0e0d8be6a3b1ae79ada5f1c3d926b4437e45097e
Aniket Pathak <74519511+aniketpathak028@users.noreply.github.com>
2022-02-23 20:08:47 +0530

    Time: 16 ms (71.02%), Space: 10.3 MB (19.73%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
-    int solve(int i, int j, int n, vector<vector<int>>& matrix, vector<vector<int>> &dp){
-        // out-of-bound condition
-        if(j<0 || j>=n) return 1e8;
-        
-        //base case
-        if(i==n-1) return matrix[i][j];
-        
-        if(dp[i][j]!=-1) return dp[i][j];
-        
-        int mini = INT_MAX;
-        for(int dj=-1; dj<=1; dj++){
-            mini = min(mini, solve(i+1, j+dj, n, matrix, dp)+matrix[i][j]);
-        }
-        
-        return dp[i][j] = mini;
-    }
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        int mini= INT_MAX;
-        int n= matrix.size();
-        vector<vector<int>> dp(n, vector<int>(n,-1));
-        for(int i=0; i<n; i++){
-            mini = min(mini, solve(0, i, n, matrix, dp));
+    int mini = INT_MAX;
+    int n = matrix.size();
+
+    // initialize DP
+    vector<vector<int>> dp(n, vector<int>(n, 0));
+
+    // base-case initialization
+    for (int i = 0; i < n; i++) dp[n-1][i]= matrix[n-1][i];
+    
+
+    for (int i = n - 2; i >= 0; i--)
+    {
+        for (int j = 0; j < n; j++){
+            int mini = INT_MAX;
+            for (int dj = -1; dj <= 1; dj++)
+            {
+                if(j+dj>=0 && j+dj<n) mini = min(mini, dp[i + 1][j + dj] + matrix[i][j]);
+            }
+
+            dp[i][j] = mini;
         }
-        
-        return mini;
+    }
+
+
+    for (int i = 0; i < n; i++) mini = min(mini, dp[0][i]);
+    
+
+    return mini;
     }
 };
