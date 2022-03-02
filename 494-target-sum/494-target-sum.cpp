f0e4b4b5422cd944d045b88ab57899d2917cb47e
Aniket Pathak <74519511+aniketpathak028@users.noreply.github.com>
2022-03-02 16:20:11 +0530

    Time: 854 ms (18.92%), Space: 9 MB (79.27%) - LeetHub

---
+++ 494-target-sum/494-target-sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int solve(int i, vector<int> &nums, int tar){
+        if(i==0){
+            if(nums[0]==0 && tar==0) return 2;
+            if(tar==nums[0] || tar==-nums[0]) return 1;
+            return 0;
+        }
+        
+        int plus = solve(i-1, nums, tar+nums[i]);
+        int minus = solve(i-1, nums, tar-nums[i]);
+        
+        return plus+minus;
+    }
+    int findTargetSumWays(vector<int>& nums, int target) {
+        return solve(nums.size()-1, nums, target);
+    }
+};
