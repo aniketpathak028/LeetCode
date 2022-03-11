967cfd2e47fb9e7871fd59bb9d22bb5ac1d8b340
Aniket Pathak <74519511+aniketpathak028@users.noreply.github.com>
2022-03-11 13:02:29 +0530

    Added solution - LeetHub

---
+++ Height Using Parent Array - GFG/height-using-parent-array.cpp
@@ -0,0 +1,35 @@
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int findHeight(int N, int arr[]){
+        vector<int> h(N, 1);
+        for(int i=1; i<N; i++) h[i]=h[arr[i]]+1;
+        return h[N-1];
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        int arr[N];
+        for(int i = 0;i < N;i++)
+            cin>>arr[i];
+        
+        Solution ob;
+        cout<<ob.findHeight(N, arr)<<"\n";
+    }
+    return 0;
+}  // } Driver Code Ends
