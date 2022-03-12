7855ff5512ec060964cd7b0ea96fbdfd4683d8db
Aniket Pathak <74519511+aniketpathak028@users.noreply.github.com>
2022-03-12 22:42:16 +0530

    Time: 325 ms (38.05%), Space: 248.3 MB (5.58%) - LeetHub

---
+++ 403-frog-jump/403-frog-jump.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool solve(int i, int last, vector<int> &stones, map<int, int> &m, vector<vector<int>> &dp){
+        // base-case
+        if(i==stones.size()-1) return true;
+        
+        if(dp[i][last]!=-1) return dp[i][last];
+        
+        int ans= false;
+        for(int di=-1; di<=1; di++){
+            if(m[stones[i]+(last+di)] && last+di>0){
+                ans = ans | solve(m[stones[i]+(last+di)], last+di, stones, m, dp);
+            }
+        }
+        
+        return dp[i][last]=ans;
+    }
+    bool canCross(vector<int>& stones) {
+        int n= stones.size();
+        map<int, int> m; // map to store all the existing elements and their indices
+        for(int i=0; i<n; i++) m[stones[i]]=i;
+        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
+        return solve(0, 0, stones, m, dp);
+    }
+};
