4ef356c03d1d1e89f17b0a914ba7472bfad78e89
Aniket Pathak <74519511+aniketpathak028@users.noreply.github.com>
2022-03-14 16:35:36 +0530

    Added solution - LeetHub

--- Minimum sum partition - GFG/minimum-sum-partition.cpp
+++ Minimum sum partition - GFG/minimum-sum-partition.cpp
@@ -6,32 +6,41 @@ using namespace std;
 class Solution{
 
   public:
-    int solve(int arr[], int n, int sum){
-        // initialize dp
-        vector<vector<bool>> dp(n, vector<bool>(sum+1, false));
-        // base-case initialization
-        for(int i=0; i<n; i++) dp[i][0]= true;
-        if(arr[0]<=sum) dp[0][arr[0]]= true;
-        // tabulation
-        for(int i=1; i<n; i++){
-            for(int j=1; j<=sum; j++){
-                bool not_pick = dp[i-1][j];
+	int minDifference(int arr[], int n)  { 
+	    int tot_sum = 0;
+        for (int i = 0; i < n; i++)
+            tot_sum += arr[i];
+    
+        int sum = tot_sum;
+        vector<vector<bool>> dp(n, vector<bool>(sum + 1, false));
+        // base case intialization
+        for (int i = 0; i < n; i++)
+            dp[i][0] = true;
+    
+        dp[0][arr[0]] = true;
+    
+        int mini = INT_MAX;
+    
+        for (int i = 1; i < n; i++)
+        {
+            for (int j = 0; j <= sum; j++)
+            {
+                bool not_pick = dp[i - 1][j];
                 bool pick = false;
-                if(arr[i]<=j) pick = dp[i-1][j-arr[i]];
-                
-                dp[i][j]= pick | not_pick;
+                if (arr[i] <= j)
+                    pick = dp[i - 1][j - arr[i]];
+    
+                dp[i][j] = pick | not_pick;
             }
         }
-        
-        int mini = INT_MAX;
-        for(int i=0; i<=sum; i++) if(dp[n-1][i]) mini= min(mini, abs((sum-i)-i));
-        
+    
+        for (int i = 0; i <= sum; i++){
+            if(dp[n-1][i]) {
+                    mini = min(mini, abs((tot_sum - i) - i));
+            }
+        }
+    
         return mini;
-    }
-	int minDifference(int arr[], int n)  { 
-	    int tot_sum= 0;
-	    for(int i=0; i<n; i++) tot_sum+=arr[i];
-	    return solve(arr, n, tot_sum);
 	} 
 };
 
