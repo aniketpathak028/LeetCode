051fafb4089c40e5acaa750f1a48d31f6af18274
Aniket Pathak <74519511+aniketpathak028@users.noreply.github.com>
2022-03-20 12:29:56 +0530

    Time: 1387 ms (11.11%), Space: 25.9 MB (100.00%) - LeetHub

---
+++ 2209-minimum-white-tiles-after-covering-with-carpets/2209-minimum-white-tiles-after-covering-with-carpets.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int dp[1001][1001];
+    int sfx[1001];
+    void findSuffix(string &floor){
+        int n= floor.size();
+        sfx[n-1]= (floor[n-1]=='1');
+        for(int i=n-2; i>=0; i--) sfx[i]= sfx[i+1] + (floor[i]=='1');
+    }
+    int solve(string &floor, int numCarpets, int carpetLen, int i, int used){
+        if (i >= floor.size()) {
+            return 0;
+        } else if (used == numCarpets) {
+            return sfx[i];
+        }
+        if(dp[i][used]!=-1) return dp[i][used];
+        return dp[i][used]= min(solve(floor, numCarpets, carpetLen, i+carpetLen, used+1), (floor[i]=='1')+solve(floor, numCarpets, carpetLen, i+1, used));
+    }
+    int minimumWhiteTiles(string floor, int numCarpets, int carpetLen) {
+        memset(dp, -1, sizeof(dp));
+        findSuffix(floor);
+        return solve(floor, numCarpets, carpetLen, 0, 0);
+    }
+};
