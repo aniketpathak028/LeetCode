34fb219a02a5348ee49e760d8544f63e742c85ff
Aniket Pathak <74519511+aniketpathak028@users.noreply.github.com>
2022-04-21 22:11:46 +0530

    Time: 341 ms (16.20%), Space: 42.8 MB (49.76%) - LeetHub

--- 721-accounts-merge/721-accounts-merge.cpp
+++ 721-accounts-merge/721-accounts-merge.cpp
@@ -1,61 +1,46 @@
 class Solution {
 public:
-    map<string,string> Parent;
-    map<string,string> Owner;
-    map<string, set<string>> unions;
-
-    string FindPar(string node)
-    {
-        if (node == Parent[node])
-            return node;
-        return Parent[node] = FindPar(Parent[node]);
+    string findParent(string acc, map<string,string> &parent){
+      if(acc==parent[acc]) return acc;
+      return parent[acc]= findParent(parent[acc], parent);
     }
 
-    void MakeSet(vector<vector<string>>& accounts)
-    {
-        for(int i=0; i<accounts.size(); ++i){
-          string owner= accounts[i][0];
-          for(int j=1; j<accounts[i].size(); ++j){
-            Parent[accounts[i][j]]= accounts[i][j];
-            Owner[accounts[i][j]]= owner;
-          }
-        }
-    }
-
-    void Union(string u, string v)
-    {
-         u = FindPar(u);
-         v = FindPar(v);
-
-        Parent[u]= v;
-    }
-  
     vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
-        // create disjoint sets
-        MakeSet(accounts);
-        // take union of commom accounts
+        map<string,string> parent;
+        map<string,string> owner;
+        map<string, set<string>> uni;
+        
+        // create disjoint sets for all accounts
         for(int i=0; i<accounts.size(); ++i){
-          string par= accounts[i][1];
-          for(int j=2; j<accounts[i].size(); ++j){
-            Union(par, accounts[i][j]);
-          }
-        }
-        // insert into union set
-        for(int i=0; i<accounts.size(); ++i){
+          string own= accounts[i][0];
           for(int j=1; j<accounts[i].size(); ++j){
-            unions[FindPar(accounts[i][j])].insert(accounts[i][j]);
+            parent[accounts[i][j]]= accounts[i][j];
+            owner[accounts[i][j]]= own;
           }
         }
+      
+        // perform union of same accounts
+        for(int i=0; i<accounts.size(); ++i){
+          string par= findParent(accounts[i][1], parent);
+          for(int j=2; j<accounts[i].size(); ++j){
+            parent[findParent(accounts[i][j], parent)]= par;
+          }
+        }
+      
+        // add the accnts to union set
+        for(auto acc: accounts){
+          for(int j=1; j<acc.size(); ++j) uni[findParent(acc[j], parent)].insert(acc[j]);
+        }
+      
+        // create ans vector
         vector<vector<string>> ans;
-        for(auto it: unions){
-          string par= it.first, own= Owner[par];
-          vector<string> accnt;
-          accnt.push_back(own);
-          for(auto s: it.second){
-            accnt.push_back(s);
-          }
-          ans.push_back(accnt);
+        for(auto &it: uni){
+          vector<string> acc;
+          acc.push_back(owner[it.first]);
+          for(auto &s: it.second) acc.push_back(s); 
+          ans.push_back(acc);
         }
+      
       return ans;
     }
 };
