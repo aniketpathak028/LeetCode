2af7aa12aa224d0547849b4d0213683bdf117c05
Aniket Pathak <74519511+aniketpathak028@users.noreply.github.com>
2022-05-10 12:45:42 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (37.26%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.cpp
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void solve(int i, int sum, int k, vector<int> comb, vector<vector<int>> &ans){
+    void solve(int i, int sum, int k, vector<int> &comb, vector<vector<int>> &ans){
       // if the target sum==0 and k digits are used
       if(sum==0 && k==0){
         ans.push_back(comb);
@@ -11,14 +11,16 @@ public:
     
       if(sum==0 || k==0) return; // if the sum is exhausted or k digits have already been used
       
-      // not-pick
-      solve(i+1, sum, k, comb, ans);
-      
       // pick 
       if(i<=sum){
         comb.push_back(i);
         solve(i+1, sum-i, k-1, comb, ans);
+        comb.pop_back();
       } 
+      
+      // not-pick
+      
+      solve(i+1, sum, k, comb, ans);
     }
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<vector<int>> ans;
