5197d70c9a9b288406454d2c30d6ee3e9af88310
Aniket Pathak <74519511+aniketpathak028@users.noreply.github.com>
2022-05-11 09:51:07 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (22.78%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
-    int count(int i, int n, string &vowels, vector<vector<int>> &dp){
-      if(n==0) return 1; // if a string of length n is formed
-      
-      if(i>=5) return 0; // if i exceeds the length of vowels
-      
-      if(dp[i][n]!=-1) return dp[i][n];
-      
-      int pick, notPick;
-      // pick i.e. pick this alphabet
-      pick= count(i, n-1, vowels, dp);
-      // notPick i.e. skip this alphabet
-      notPick= count(i+1, n, vowels, dp);
-      
-      return dp[i][n]= pick + notPick;
-    }
-  
     int countVowelStrings(int n) {
+        
         string vowels= "aeiou"; // all vowels in lexicographically sorted order
-        vector<vector<int>> dp(5, vector<int>(n+1, -1));
-        return count(0, n, vowels, dp);
+        
+        vector<vector<int>> dp(5, vector<int>(n+1)); // dp vector
+      
+        // base-case
+        for(int i=0; i<5; ++i) dp[i][0]= 1; // if n==0 return 1
+      
+        // tabulation
+        for(int i=4; i>=0; --i){
+          for(int j=1; j<=n; ++j){
+            int pick= 0, notPick= 0;
+            // pick i.e. pick this alphabet
+            pick= dp[i][j-1];
+            // notPick i.e. skip this alphabet
+            if(i<4) notPick= dp[i+1][j];
+            
+            dp[i][j]= pick+notPick;
+          }
+        }
+      
+        return dp[0][n]; // return all strings possible starting from 0 index and of n length
     }
 };
