0c7b6d7843b64dfd38fa88afb48004d95f8bf823
Aniket Pathak <74519511+aniketpathak028@users.noreply.github.com>
2022-05-15 11:16:46 +0530

    Time: 129 ms (51.67%), Space: 59.8 MB (53.32%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -11,22 +11,24 @@
  */
 class Solution {
 public:
+    pair<int,int> dfs(TreeNode* root){
+      if(!root) return {0,0}; // if null encountered return null pair
+      
+      // leaf-node
+      if(!root->left && !root->right){
+          return {root->val, 1};
+      }
+      // non-leaf-node
+      else{
+        auto left= dfs(root->left), right= dfs(root->right);
+        if(left.second==right.second) return {left.first+right.first, left.second+1};
+        else if(left.second>right.second) return {left.first, left.second+1};
+        else return {right.first, right.second+1};
+      }
+    }
+  
     int deepestLeavesSum(TreeNode* root) {
-        vector<vector<int>> ans;
-        queue<TreeNode*> q;
-        q.push(root);
-        while(!q.empty()){
-          int size= q.size();
-          vector<int> level;
-          for(int i=0; i<size; ++i){
-            auto node= q.front();
-            q.pop();
-            level.push_back(node->val);
-            if(node->left) q.push(node->left);
-            if(node->right) q.push(node->right);
-          }
-          ans.push_back(level);
-        }
-        return accumulate(ans[ans.size()-1].begin(), ans[ans.size()-1].end(), 0);
+        auto ans= dfs(root);
+        return ans.first;
     }
 };
