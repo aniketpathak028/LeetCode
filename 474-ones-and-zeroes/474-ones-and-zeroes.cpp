98f7f47c69b555bce33a359c0cb86de8a261c14b
Aniket Pathak <74519511+aniketpathak028@users.noreply.github.com>
2022-05-23 22:09:06 +0530

    Time: 318 ms (62.15%), Space: 100.1 MB (25.94%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -1,40 +1,32 @@
 class Solution {
 public:
-    int solve(int i, vector<pair<int,int>> &count, int m, int n, vector<vector<vector<int>>> &dp){
-      // if m 0's or n 1's are used
-      if(m==0 && n==0) return 0;
-      // base-case, if i exceeds the str vector
-      if(i==count.size()) return 0;
+    // Recurrence
+    int solve(int i, vector<string> &str, int n, int m, vector<vector<vector<int>>> &dp){
+      // base-case, when i goes out of bound OR we have used all the 1's and 0's
+      if(i==str.size() || (!m && !n)) return 0;
       
-      // check if dp contains the ans
-      if(dp[i][m][n]!=-1) return dp[i][m][n];
+      // check if dp has ans
+      if(dp[i][n][m]!=-1) return dp[i][n][m];
+      
+      // calculate the no of 0's and 1's in the current string
+      int cnt_0= 0, cnt_1= 0;
+      for(auto ch: str[i]) cnt_1+= ch-'0';
+      cnt_0= str[i].size()- cnt_1;
       
       int pick, notPick;
       pick= notPick= INT_MIN;
       
-      int cnt_1= count[i].second, cnt_0= count[i].first;
       // pick 
-      if(cnt_0<=m && cnt_1<=n)
-        pick= 1+solve(i+1, count, m-cnt_0, n-cnt_1, dp);
-      // notPick
-      notPick= solve(i+1, count, m, n, dp);
+      if(cnt_0<=m && cnt_1<=n) pick= 1+solve(i+1, str, n-cnt_1, m-cnt_0, dp);
+      // notpick
+      notPick= solve(i+1, str, n, m, dp);
       
-      return dp[i][m][n]= max(pick, notPick);
+      return dp[i][n][m]= max(pick, notPick);
     }
   
     int findMaxForm(vector<string>& strs, int m, int n) {
-      int i= strs.size();
-      // store the count of 0 and 1 in all strings 
-      vector<pair<int,int>> count;
-      for(auto s: strs){
-        int cnt_1= 0, cnt_0= 0;
-        // count all 0's and 1's
-        for(int j=0; j<s.size(); ++j) cnt_1+= s[j]-'0';
-        cnt_0 = s.size() - cnt_1;
-        count.push_back({cnt_0, cnt_1});
-      }
-      
-      vector<vector<vector<int>>> dp(i, vector<vector<int>>(m+1, vector<int>(n+1, -1)));
-      return solve(0, count, m, n, dp);
+        int i= strs.size();
+        vector<vector<vector<int>>> dp(i, vector<vector<int>>(n+1, vector<int>(m+1, -1)));
+        return solve(0, strs, n, m, dp);
     }
 };
