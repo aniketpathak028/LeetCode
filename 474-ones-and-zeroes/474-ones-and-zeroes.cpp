dd28aaa4dd5e040b4ab5e7d1eb96c513b2fdb8be
Aniket Pathak <74519511+aniketpathak028@users.noreply.github.com>
2022-05-23 22:40:26 +0530

    Time: 776 ms (14.42%), Space: 100.6 MB (25.61%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
-    // Recurrence
-    int solve(int i, vector<string> &str, int n, int m, vector<vector<vector<int>>> &dp){
-      // base-case, when i goes out of bound OR we have used all the 1's and 0's
-      if(i==str.size() || (!m && !n)) return 0;
-      
-      // check if dp has ans
-      if(dp[i][n][m]!=-1) return dp[i][n][m];
-      
-      // calculate the no of 0's and 1's in the current string
-      int cnt_0= 0, cnt_1= 0;
-      for(auto ch: str[i]) cnt_1+= ch-'0';
-      cnt_0= str[i].size()- cnt_1;
-      
-      int pick, notPick;
-      pick= notPick= INT_MIN;
-      
-      // pick 
-      if(cnt_0<=m && cnt_1<=n) pick= 1+solve(i+1, str, n-cnt_1, m-cnt_0, dp);
-      // notpick
-      notPick= solve(i+1, str, n, m, dp);
-      
-      return dp[i][n][m]= max(pick, notPick);
-    }
-  
+    // tabulation
     int findMaxForm(vector<string>& strs, int m, int n) {
-        int i= strs.size();
-        vector<vector<vector<int>>> dp(i, vector<vector<int>>(n+1, vector<int>(m+1, -1)));
-        return solve(0, strs, n, m, dp);
+        int s= strs.size();
+        // dp vector
+        vector<vector<vector<int>>> dp(s+1, vector<vector<int>>(n+1, vector<int>(m+1, 0)));
+        // tabulation
+        for(int i=s-1; i>=0; --i){
+         for(int j=0; j<=n; ++j){
+           for(int k=0; k<=m; ++k){
+              if(j==0 && k==0) continue; // base case both n==0 && m==0
+              // calculate the no of 0's and 1's in the current string
+              int cnt_0= 0, cnt_1= 0;
+              for(auto ch: strs[i]) cnt_1+= ch-'0';
+              cnt_0= strs[i].size()- cnt_1;
+    
+              int pick, notPick;
+              pick= notPick= INT_MIN;
+
+              // pick 
+              if(cnt_0<=k && cnt_1<=j) pick= 1+dp[i+1][j-cnt_1][k-cnt_0];
+              // notpick
+              notPick= dp[i+1][j][k];
+
+              dp[i][j][k]= max(pick, notPick);
+           }
+         } 
+        }
+      
+      return dp[0][n][m];
     }
 };
