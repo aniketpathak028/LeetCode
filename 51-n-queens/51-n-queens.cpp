44b48e9d9a5cc76465c20a135f4c7293b08d9ea5
Aniket Pathak <74519511+aniketpathak028@users.noreply.github.com>
2022-06-04 11:35:18 +0530

    Time: 1309 ms (5.06%), Space: 236.3 MB (5.44%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,48 +1,51 @@
 class Solution {
 public:
-  
-    bool isSafe(int i, int j, vector<string> &board, map<int,int> &mp_top_down, map<int,int> &mp_bottom_up){
-      // check vertical 
-      for(int i1=i; i1>=0; i1--)
-        if(board[i1][j]=='Q') return false;
-      // check diagonals
-      if(mp_top_down[i-j]==1 || mp_bottom_up[i+j]==1) return false;
+    bool isSafe(int i, int j, vector<string> &board, map<int,int> m1, map<int,int> m2){
+      // check column above
+      for(int i1= i; i1>=0; --i1) if(board[i1][j]=='Q') return false;
+      // check for diagonals
+      if(m1[i-j] || m2[i+j]) return false;
       return true;
     }
-    
-    void findAllSolutions(int i, vector<string> &board, vector<vector<string>> &ans, int n, map<int,int> &mp_top_down, map<int,int> &mp_bottom_up){
-      // base-case, board exceeded hence a solution must have been found
+  
+    void findAllSolutions(int i, vector<string> &board, vector<vector<string>> &ans, map<int,int> &m1, map<int,int> &m2, int n){
+      // if the board is exceeded, a solution must have been found
       if(i==n){
-        // insert the board into ans
         ans.push_back(board);
         return;
       }
       
-      // for a given row, check all the columns if it is valid to insert a queen
+      // iterate for all columns and check if a Queen can be placed
       for(int j=0; j<n; ++j){
-        if(isSafe(i, j, board, mp_top_down, mp_bottom_up)){
+        if(isSafe(i, j, board, m1, m2)){
+          // if it is safe to place a Queen, place a queen and then backtrack
           board[i][j]= 'Q';
-          mp_top_down[i-j]= 1;
-          mp_bottom_up[j+i]= 1;
-          findAllSolutions(i+1, board, ans, n, mp_top_down, mp_bottom_up);
-          mp_top_down[i-j]= 0;
-          mp_bottom_up[j+i]= 0;
+          m1[i-j]= 1;
+          m2[i+j]= 1;
+          findAllSolutions(i+1, board, ans, m1, m2, n);
+          m1[i-j]= 0;
+          m2[i+j]= 0;
           board[i][j]= '.';
         }
       }
     }
-    
+  
     vector<vector<string>> solveNQueens(int n) {
-        // ans vector containing all the board possiblities for n
-        vector<vector<string>> ans;
-        // represents a row of the board
-        string row(n, '.');
-        // represents the chess board containing n rows
-        vector<string> board(n, row);
-        // maps to keep track of queens 
-        map<int,int> mp_top_down, mp_bottom_up;
-        findAllSolutions(0, board, ans, n, mp_top_down, mp_bottom_up);
+      // ans vector to store all board possiblities
+      vector<vector<string>> ans;
+    
+      // represents a row of the board
+      string row(n, '.');
       
-        return ans;
+      // nxn chess board
+      vector<string> board(n, row);
+      
+      // maps to keep track of attacked positions
+      map<int,int> m1, m2;
+      
+      // function to find all board possiblities
+      findAllSolutions(0, board, ans, m1, m2, n);
+      
+      return ans;
     }
 };
