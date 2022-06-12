2db8f886a01b62eb3015abd12a12115c2080a9aa
Aniket Pathak <74519511+aniketpathak028@users.noreply.github.com>
2022-06-12 20:53:08 +0530

    Time: 728 ms (17.99%), Space: 131.1 MB (14.10%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
@@ -1,33 +1,22 @@
 class Solution {
 public:
     int maximumUniqueSubarray(vector<int>& nums) {
-        int left= 0, right= 0, n= nums.size();
-        int max_sum= INT_MIN;
-        
-        // two pointer algorithm to find the maximum sum subarray containing non-duplicate elements
-        vector<int> mp(1e4+1, -1); // map to store an element's last seen position
-      
-        // find prefix sum
-        vector<int> prefix(n, 0);
-      
+        int left= 0, right= 0, n= nums.size(), maxi= INT_MIN;
+        unordered_set<int> set; 
+        vector<int> prefix(n);
         int sum= 0;
         for(int i=0; i<n; ++i){
           sum+= nums[i];
           prefix[i]= sum;
         }
-        
-        while(left<n && right<n){
-          // if a duplicate is found, move the left pointer to the right of its last occurence
-          if(mp[nums[right]]!=-1 && mp[nums[right]]>=left) left= mp[nums[right]]+1;  
-          
-          mp[nums[right]]= right; // update the new position of the element
-          
-          // find the new sum
-          max_sum= max(max_sum, prefix[right]-prefix[left]+nums[left]);
-          
-          right++;
+        while(right<n){
+          if(set.count(nums[right]))
+            set.erase(nums[left++]);
+          else if(!set.count(nums[right])){
+            set.insert(nums[right]);
+            maxi= max(maxi, prefix[right++]-prefix[left]+nums[left]);
+          }
         }
-      
-        return max_sum;
+      return maxi;
     }
 };
