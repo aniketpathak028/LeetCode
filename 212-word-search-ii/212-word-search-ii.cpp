4fd2384482c43ea316d7c95a6e65beb9fcb550c0
Aniket Pathak <74519511+aniketpathak028@users.noreply.github.com>
2022-07-02 22:25:31 +0530

    Time: 2912 ms (5.03%), Space: 13 MB (75.43%) - LeetHub

--- 212-word-search-ii/212-word-search-ii.cpp
+++ 212-word-search-ii/212-word-search-ii.cpp
@@ -1,42 +1,83 @@
-struct TrieNode {
-    TrieNode* children[26] = {};
-    string* word;
-    void addWord(string& word) {
-        TrieNode* cur = this;
-        for (char c : word) {
-            c -= 'a';
-            if (cur->children[c] == nullptr) cur->children[c] = new TrieNode();
-            cur = cur->children[c];
-        }
-        cur->word = &word;
+struct Node{
+  Node* links[26];
+  bool flag;
+  
+  bool containsKey(char ch){
+    return links[ch-'a']!=NULL;
+  }
+  
+  void createRef(char ch){
+    links[ch-'a']= new Node();
+  }
+  
+  Node* getNextNode(char ch){
+    return links[ch-'a'];
+  }
+};
+
+class Trie{
+  private:
+    Node* root;
+    vector<vector<int>> dir= {{0,1}, {1,0}, {-1,0}, {0,-1}};
+  
+  public:
+    Trie(){
+      root= new Node();    
+    }
+  
+    void insert(string word){
+      Node* node= root;
+      for(int i=0; i<word.size(); ++i){
+        if(!node->containsKey(word[i])) node->createRef(word[i]);
+        node= node->getNextNode(word[i]);
+      }
+      node->flag= true;
+    }
+  
+    void solve(int i, int j, vector<vector<char>> &board, vector<string> &ans){
+      dfs(i, j, root, board, "", ans);
+    }
+
+    void dfs(int i, int j, Node* node, vector<vector<char>> &b, string word, vector<string> &ans){
+      // array out of bound or trie doesn't contain character or visited cell
+      if(i<0 || j<0 || i==b.size() || j==b[0].size() || b[i][j]=='#' || !node->containsKey(b[i][j])) return;
+      
+      // add the current charcter to the word
+      word+= b[i][j];
+      
+      // if end of a word is reached
+      node= node->getNextNode(b[i][j]);
+      if(node->flag){
+        ans.push_back(word);
+        node->flag= false;
+      }
+  
+      char ch= b[i][j];
+      // apply dfs in all 4 directions
+      for(auto &d: dir){
+        b[i][j]= '#';
+        dfs(i+d[0], j+d[1], node, b, word, ans);
+        b[i][j]= ch;
+      }
     }
 };
 
 class Solution {
 public:
-    int m, n;
-    int DIR[5] = {0, 1, 0, -1, 0};
-    vector<string> ans;
     vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
-        m = board.size(); n = board[0].size();
-        TrieNode trieNode;
-        for (string& word : words) trieNode.addWord(word);
+        Trie trie;
+        vector<string> ans;
         
-        for (int r = 0; r < m; ++r)
-            for (int c = 0; c < n; ++c)
-                dfs(board, r, c, &trieNode);
-        return ans;
-    }
-    void dfs(vector<vector<char>>& board, int r, int c, TrieNode* cur) {
-        if (r < 0 || r == m || c < 0 || c == n || board[r][c] == '#' || cur->children[board[r][c]-'a'] == nullptr) return;
-        char orgChar = board[r][c];
-        cur = cur->children[orgChar - 'a'];
-        if (cur->word != nullptr) {
-            ans.push_back(*cur->word);
-            cur->word = nullptr; // Avoid duplication!
+        for(auto &word: words) trie.insert(word);
+      
+        int n= board.size(), m= board[0].size();
+        
+        for(int i=0; i<n; ++i){
+          for(int j=0; j<m; ++j){
+            trie.solve(i, j, board, ans);
+          }
         }
-        board[r][c] = '#'; // mark as visited!
-        for (int i = 0; i < 4; ++i) dfs(board, r + DIR[i], c + DIR[i+1], cur);
-        board[r][c] = orgChar; // restore org state
+      
+      return ans;
     }
 };
