8898b9a2888d956953e41031812325bebb3baafb
Aniket Pathak <74519511+aniketpathak028@users.noreply.github.com>
2022-07-16 21:42:37 +0530

    Time: 5 ms (93.79%), Space: 9.4 MB (46.15%) - LeetHub

--- 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
+++ 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
@@ -1,21 +1,27 @@
-
-    class Solution {
+class Solution {
 public:
-    int m, n;
-    int memo[50][50][51];
-    int DIR[5] = {0, 1, 0, -1, 0};
-    int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
-        this->m = m; this->n = n;
-        memset(memo, -1, sizeof(memo));
-        return dp(startRow, startColumn, maxMove);
+    int mod= 1e9+7;
+  
+    int solve(int i, int j, int m, int n, int moves, vector<vector<vector<int>>> &dp){
+      // if array out of bound add a move
+      if(i<0 || j<0 || i==m || j==n) return 1;
+      
+      if(!moves) return 0; // if moves over return 0 moves
+      
+      if(dp[i][j][moves]!=-1) return dp[i][j][moves];
+      
+      long long up, down, left, right;
+      
+      up= solve(i-1, j, m, n, moves-1, dp);
+      down= solve(i+1, j, m, n, moves-1, dp);
+      right= solve(i, j+1, m, n, moves-1, dp);
+      left= solve(i, j-1, m, n, moves-1, dp);
+      
+      return dp[i][j][moves]= (up+down+left+right)%mod;
     }
-    int dp(int r, int c, int maxMove) {
-        if (r < 0 || r == m || c < 0 || c == n) return 1; // Out of bound -> Count 1 way
-        if (maxMove == 0) return 0;
-        if (memo[r][c][maxMove] != -1) return memo[r][c][maxMove];
-        int ans = 0;
-        for (int i = 0; i < 4; ++i)
-            ans = (ans + dp(r + DIR[i], c + DIR[i+1], maxMove - 1)) % 1000000007;
-        return memo[r][c][maxMove] = ans;
+  
+    int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+        vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int>(maxMove+1, -1)));
+        return solve(startRow, startColumn, m, n, maxMove, dp);
     }
 };
