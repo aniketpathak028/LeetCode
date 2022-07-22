49b7a89dcc56de4a5aadfca41686dc01e82dc605
Aniket Pathak <74519511+aniketpathak028@users.noreply.github.com>
2022-07-22 20:06:33 +0530

    Time: 838 ms (72.04%), Space: 209.9 MB (29.94%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
-    bool dfs(int i, int d, vector<int> &vis, vector<int> adj[]){
-        if(i==d) return true;
-        vis[i]= 1;
-        
-        for(auto it: adj[i]){
-            if(!vis[it]){
-                if(dfs(it, d, vis, adj)) return true;
-            }
+    bool dfs(int node, int dest, vector<int> &vis, vector<int> adj[]){
+      if(node==dest) return true;
+      if(!vis[node]){
+        vis[node]= 1;
+        for(auto &it: adj[node]){
+          if(dfs(it, dest, vis, adj)) return true;
         }
-        
-        return false;
-        
+      }
+      return false;
     }
+  
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+        // create adjacency list
         vector<int> adj[n];
-        vector<int> vis(n, 0);
-        for(int i=0; i<edges.size(); i++){
-            int u = edges[i][0];
-            int v = edges[i][1];
-            adj[u].push_back(v);
-            adj[v].push_back(u);
+        for(auto &edge: edges){
+          adj[edge[0]].push_back(edge[1]);
+          adj[edge[1]].push_back(edge[0]);
         }
+        // run dfs to check the total number of valid paths
+        vector<int> vis(n);
+      
         return dfs(source, destination, vis, adj);
     }
 };
